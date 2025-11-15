15456478164be1f98bbf167352564c5cfd003e4d
ChengRong <crkee.2022@scis.smu.edu.sg>
2025-11-15 19:38:20 +0800

    cleanup

--- telegram-bot/db.js
+++
@@ -1,6 +0,0 @@
-// Import Prisma client from parent directory
-const { PrismaClient } = require('../node_modules/@prisma/client');
-
-const prisma = new PrismaClient();
-
-module.exports = { prisma };
